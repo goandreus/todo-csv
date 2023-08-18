ab6ec78853bcadce5510e2e6fa9acfb2d82eecfe
goandreus <andres.chavez.crisostomo@gmail.com>
2023-08-18 13:26:27 -0500

    feat: function read and wrap csv result

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use csv::{ReaderBuilder, StringRecord};
+use std::{fs};
+
+const FILENAME: &str ="history.csv";
+
+fn main() {
+    let content: String = fs::read_to_string(FILENAME).unwrap();
+    let mut rdr = ReaderBuilder::new().delimiter(b';').from_reader(content.as_bytes());
+
+    for result in rdr.records(){
+        print!("Texto: {}", result.unwrap().get(2).unwrap().trim());
+    }
+}
